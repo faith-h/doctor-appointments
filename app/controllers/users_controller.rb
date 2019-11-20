63c6a774c387e2e81ff3b5bd0e978b7ce8e25f19
Faith <faithhafen@gmail.com>
2019-11-19 21:48:29 -0700

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = current_user.User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      redirect_to users_path
     else
       render :new
     end
